a878f6137477fcb31de32acef58c3fc50b016b3b
Sam Johns <Sam@pc-70-2.customer.crescentplace.studenthousingcompany.lan>
2019-05-07 23:51:43 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// function showDropDown(button) {
+//     console.log("Button clicked");
+//     var dropDowns = document.getElementsByClassName("drop_down_list");
+//     var i;
+//     for (i = 0; i < dropDowns.length; i++) {
+//         dropDowns[i].classList.remove("show");
+//     }
+//     button.nextSibling.nextSibling.classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//     if (!event.target.matches(".btn")) {
+//         var dropDowns = document.getElementsByClassName("drop_down_list");
+//         var i;
+//         for (i = 0; i < dropDowns.length; i++) {
+//             var visibleDropDown = dropDowns[i];
+//             if (visibleDropDown.classList.contains("show")) {
+//                 visibleDropDown.classList.remove("show");
+//             }
+//         }
+//     }
+// }
+
